ce5f77cc327fa3a9761518bbea8e6357a55e88d8
Andres Aguilar <aguilarcarboni@gmail.com>
2024-08-27 13:32:05 -0600

    Create InitializeGrid function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+static class Program {
+    const int GRID_SIZE = 10;
+    const int NUM_SHIPS = 3;
+    static int[,] grid = new int[GRID_SIZE, GRID_SIZE];
+    static int[] shipSizes = { 2, 3, 4 };
+    static int shipsRemaining = NUM_SHIPS;
+
+    // Main
+    static void Main(string[] args) {
+
+        InitializeGrid();
+        
+    }
+    static void InitializeGrid() {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                grid[i, j] = 0;
+            }
+        }
+    }
+}
