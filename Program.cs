89ee707fe018cb97a1769beb952c1609e74d9a8d
Andres Aguilar <aguilarcarboni@gmail.com>
2024-08-28 10:43:16 -0600

    Add ships and placing functionality

--- Program.cs
+++ Program.cs
@@ -7,13 +7,18 @@ static class Program {
     static int[] shipSizes = { 2, 3, 4 };
     static int shipsRemaining = NUM_SHIPS;
 
+    static bool development = true;
+
     // Main
     static void Main(string[] args) {
 
         InitializeGrid();
-        PrintGrid();
-        
+        PlaceShips(grid);
+
+        PrintGrid(grid);
     }
+
+    // Grid
     static void InitializeGrid() {
         for (int i = 0; i < GRID_SIZE; i++) {
             for (int j = 0; j < GRID_SIZE; j++) {
@@ -21,7 +26,7 @@ static class Program {
             }
         }
     }
-    static void PrintGrid() {
+    static void PrintGrid(int[,] grid) {
         Console.WriteLine("  0 1 2 3 4 5 6 7 8 9");
         for (int i = 0; i < GRID_SIZE; i++) {
             Console.Write(i + " ");
@@ -29,6 +34,9 @@ static class Program {
             {
                 if (grid[i, j] < 0) {
                     Console.Write("X ");
+                }
+                else if (grid[i, j] > 0 && development) {
+                    Console.Write(grid[i, j] + " ");
                 }
                 else if (grid[i, j] == -9) {
                     Console.Write("O ");
@@ -40,4 +48,54 @@ static class Program {
             Console.WriteLine();
         }
     }
+    static void PlaceShips(int[,] grid) {
+        Random random = new Random();
+        
+        for (int i = 0; i < NUM_SHIPS; i++) {
+            bool placed = false;
+            while (!placed) {
+
+                // Place ship in random location
+                int row = random.Next(GRID_SIZE);
+                int col = random.Next(GRID_SIZE);
+                bool horizontal = random.Next(2) == 0;
+                
+                // Do I need to pass grid? How to handle as class?
+                if (CanPlaceShip(grid, row, col, shipSizes[i], horizontal)) {
+                    PlaceShip(grid, row, col, shipSizes[i], horizontal);
+                    placed = true;
+                }
+            }
+        }
+    }
+
+    // Ship
+    static void PlaceShip(int[,] grid, int row, int col, int size, bool horizontal) {
+        if (horizontal) {
+            for (int i = 0; i < size; i++) {
+                grid[row, col + i] = size;
+            }
+        }
+        else {
+            for (int i = 0; i < size; i++) {
+                grid[row + i, col] = size;
+            }
+        }
+    }
+    static bool CanPlaceShip(int[,] grid, int row, int col, int size, bool horizontal) {
+        if (horizontal) {
+            if (col + size > GRID_SIZE) return false;
+            for (int i = 0; i < size; i++) {
+                if (grid[row, col + i] != 0) return false;
+            }
+        }
+        else {
+            if (row + size > GRID_SIZE) return false;
+            for (int i = 0; i < size; i++) {
+                if (grid[row + i, col] != 0) return false;
+            }
+        }
+        return true;
+    }
+    
 }
--- obj/Debug/net8.0/battleship.AssemblyInfo.cs
+++ obj/Debug/net8.0/battleship.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("battleship")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5f77cc327fa3a9761518bbea8e6357a55e88d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a8f76584727e6e4dd453eebb8225ac3943a399a")]
 [assembly: System.Reflection.AssemblyProductAttribute("battleship")]
 [assembly: System.Reflection.AssemblyTitleAttribute("battleship")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
